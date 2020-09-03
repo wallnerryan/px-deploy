9988f9235cd9e09f3cd5dfbdd2b849170fab678f
wallnerryan <ryan.wallner@portworx.com>
2020-09-03 09:43:51 -0400

    revert to orignal

--- install.sh
+++ install.sh
@@ -9,8 +9,7 @@ NC='\033[0m'
 echo -e ${BLUE}Setting up installation container
 yum install -y git docker >&/dev/null
 echo Cloning repo
-#git clone https://github.com/andrewh1978/px-deploy >&/dev/null
-git clone --branch metro-plus-pxbackup https://github.com/wallnerryan/px-deploy
+git clone https://github.com/andrewh1978/px-deploy >&/dev/null
 cd px-deploy
 echo Building container
 docker build -t px-deploy . >&/dev/null
